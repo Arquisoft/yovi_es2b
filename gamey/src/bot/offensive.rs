fb5c4d42d9052d05a37f90cbbfe8e0a6f3ba5f53
Iyán Iglesias Ibaseta <iyaniglesiasibaseta@gmail.com>
2026-03-19 20:55:46 +0100

    Creacion algoritomo bot ofensivo

--- gamey/src/bot/offensive.rs
+++ gamey/src/bot/offensive.rs
@@ -1,29 +1,10 @@
-//! A simple random bot implementation.
-//!
-//! This module provides [`RandomBot`], a bot that makes random valid moves.
-//! It is useful for testing and as a baseline opponent.
+// Bot que sigue estrategia ofensiva
+// Se centra en priorizar casillas con mas contacto
 
 use crate::{Coordinates, GameY, YBot};
 use rand::prelude::IndexedRandom;
 
-/// A bot that chooses moves randomly from the available cells.
-///
-/// This is the simplest possible bot implementation - it simply picks
-/// a random empty cell on the board. While not strategic, it serves as
-/// a useful baseline and testing tool.
-///
-/// # Example
-///
-/// ```
-/// use gamey::{GameY, RandomBot, YBot};
-///
-/// let bot = RandomBot;
-/// let game = GameY::new(5);
-///
-/// // The bot will always return Some when there are available moves
-/// let chosen_move = bot.choose_move(&game);
-/// assert!(chosen_move.is_some());
-/// ```
+
 pub struct OffensiveBot;
 
 impl YBot for OffensiveBot {
@@ -33,9 +14,49 @@ impl YBot for OffensiveBot {
 
     fn choose_move(&self, board: &GameY) -> Option<Coordinates> {
         let available_cells = board.available_cells();
-        let cell = available_cells.choose(&mut rand::rng())?;
-        let coordinates = Coordinates::from_index(*cell, board.board_size());
-        Some(coordinates)
+
+        //Si no hay casillas libres retorna
+        if available_cells.is_empty() {
+            return None;
+        }
+
+        // Buscamos casilla con mas lados tocados
+        let mut best_score = 0;
+        // &idx para tener el valor directamente
+        for &idx in available_cells {
+            let coords = Coordinates::from_index(idx, board.board_size());
+            let score = sides_touched(coords);
+            if score > best_score {
+                best_score = score;
+            }
+        }
+
+        // Recogemos todas las casillas con esa puntuacion
+        let mut best_cells: Vec<u32> = Vec::new();
+        for &idx in available_cells {
+            let coords = Coordinates::from_index(idx, board.board_size());
+            if sides_touched(coords) == best_score {
+                best_cells.push(idx);
+            }
+        }
+
+        //Se elige una casilla random para evitar que siempre pueda seguir el mismo patron en 
+        // partidas sucesivas
+        let cell = best_cells.choose(&mut rand::rng())?;
+        Some(Coordinates::from_index(*cell, board.board_size()))
     }
 }
 
+fn sides_touched(coords: Coordinates) -> u32 {
+    let mut count = 0;
+    if coords.touches_side_a() {
+        count += 1;
+    }
+    if coords.touches_side_b() {
+        count += 1;
+    }
+    if coords.touches_side_c() {
+        count += 1;
+    }
+    count
+}
