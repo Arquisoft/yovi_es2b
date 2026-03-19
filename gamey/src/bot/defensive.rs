238c7dc54f155312217b7e49627e2df8d47f0a44
Iyán Iglesias Ibaseta <iyaniglesiasibaseta@gmail.com>
2026-03-19 20:56:18 +0100

    Creacion algoritomo bot defensivo

--- gamey/src/bot/defensive.rs
+++ gamey/src/bot/defensive.rs
@@ -1,29 +1,10 @@
-//! A simple random bot implementation.
-//!
-//! This module provides [`RandomBot`], a bot that makes random valid moves.
-//! It is useful for testing and as a baseline opponent.
+// Bot que sigue estrategia defensiva
+// Se centra en priorizar casillas centrales
 
 use crate::{Coordinates, GameY, YBot};
 use rand::prelude::IndexedRandom;
 
-/// A bot that chooses moves randomly from the available cells.
-///
-/// This is the simplest possible bot implementation - it simply picks
-/// a random empty cell on the board. While not strategic, it serves as
-/// a useful baseline and testing tool.
-///
-/// # Example
-///
-/// ```
-/// use gamey::{GameY, RandomBot, YBot};
-///
-/// let bot = RandomBot;
-/// let game = GameY::new(5);
-///
-/// // The bot will always return Some when there are available moves
-/// let chosen_move = bot.choose_move(&game);
-/// assert!(chosen_move.is_some());
-/// ```
+
 pub struct DefensiveBot;
 
 impl YBot for DefensiveBot {
@@ -33,9 +14,39 @@ impl YBot for DefensiveBot {
 
     fn choose_move(&self, board: &GameY) -> Option<Coordinates> {
         let available_cells = board.available_cells();
-        let cell = available_cells.choose(&mut rand::rng())?;
-        let coordinates = Coordinates::from_index(*cell, board.board_size());
-        Some(coordinates)
+
+        //Si no hay casillas libres retorna
+        if available_cells.is_empty() {
+            return None;
+        }
+
+        // Buscamos la casilla mas central
+        let mut best_score = 0;
+        // &idx para tener el valor directamente
+        for &idx in available_cells {
+            let coords = Coordinates::from_index(idx, board.board_size());
+            let score = centrality(coords);
+            if score > best_score {
+                best_score = score;
+            }
+        }
+
+        // Recogemos todas las casillas con esa puntuacion
+        let mut best_cells: Vec<u32> = Vec::new();
+        for &idx in available_cells {
+            let coords = Coordinates::from_index(idx, board.board_size());
+            if centrality(coords) == best_score {
+                best_cells.push(idx);
+            }
+        }
+
+        //Se elige una casilla random para evitar que siempre pueda seguir el mismo patron en
+        // partidas sucesivas
+        let cell = best_cells.choose(&mut rand::rng())?;
+        Some(Coordinates::from_index(*cell, board.board_size()))
     }
 }
 
+fn centrality(coords: Coordinates) -> u32 {
+    coords.x().min(coords.y()).min(coords.z())
+}
