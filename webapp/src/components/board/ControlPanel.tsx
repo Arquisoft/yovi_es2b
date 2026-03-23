1c3a8024cc10cda3860e9a2423f9f06f37263c97
saranaredo <saranaredoclase@gmail.com>
2026-03-23 20:54:49 +0100

    boton terminar arreglado + boton reiniciar agregado

--- webapp/src/components/board/ControlPanel.tsx
+++ webapp/src/components/board/ControlPanel.tsx
@@ -1,7 +1,13 @@
-export default function ControlPanel({onExit} : { onExit: () => void }) {
+interface ControlPanelProps {
+  onExit: () => void;
+  onRestart: () => void;
+}
+
+export default function ControlPanel({ onExit, onRestart }: ControlPanelProps) {
 
   return (
     <div className="controls">
+      <button id="restart-button" onClick={onRestart}>Reiniciar partida</button>
       <button id="exit-button" onClick={onExit}>Terminar partida</button>
     </div>
   );
