f7acc7e343fa5575b6b3a1699984a9971dc6c294
JimenaVazquez <uo301668@uniovi.es>
2026-04-19 15:57:49 +0200

    prueba tests 3

--- users/__tests__/users-service-ranking.test.js
+++ users/__tests__/users-service-ranking.test.js
@@ -21,15 +21,31 @@ describe('GET /ranking', () => {
         app.get('/ranking/defeats',      userController.rankingdefeats)
     })
 
+    // ---------- Ranking con usuario ---------- //
+    /**
+     * Un usuario recién creado aparece en el ranking con value 0
+     */
+     it('un usuario nuevo aparece en el ranking con value 0', async () => {
+        await setup('Test_Username_Ranking', 'Test_PasswordRanking')
+ 
+        const res = await request(app)
+            .get('/ranking/wins')
+            .set('Accept', 'application/json')
+ 
+        const entry = res.body.ranking.find(e => e.username === 'Test_Username_Ranking')
+ 
+        expect(entry).toBeDefined()
+        expect(entry.value).toBe(0)
+ 
+        await takedown('Test_Username_Ranking')
+    })
+
     // ---------- Ranking por victorias ---------- //
     
     /**
      * Devuelve el ranking global por victorias correctamente
      */
     it('se obtiene el ranking por victorias', async () => {
-
-        await setup('Test_Username_Ranking_Wins', 'Test_Password_Wins')
-
         const res = await request(app)
             .get('/ranking/wins')
             .set('Accept', 'application/json')
@@ -38,7 +54,6 @@ describe('GET /ranking', () => {
         expect(res.body).toHaveProperty('ranking')
         expect(Array.isArray(res.body.ranking)).toBe(true)
 
-        await takedown('Test_Username_Ranking_Wins')
     })
  
     /**
@@ -67,7 +82,6 @@ describe('GET /ranking', () => {
             expect(entry).toHaveProperty('username')
             expect(entry).toHaveProperty('value')
             expect(entry).toHaveProperty('percentage')
-
         }
     })
 
@@ -100,7 +114,8 @@ describe('GET /ranking', () => {
         }
     })
  
-    //Ranking por derrotas 
+    
+    // ---------- Ranking por derrotas  ---------- //
 
     /**
      * Devuelve el ranking global por derrotas correctamente
